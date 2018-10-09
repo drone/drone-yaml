05bf5dcb396c4088ea37a9e810971b36a6a1ef12
Brad Rydzewski <brad.rydzewski@gmail.com>
2018-10-09 13:21:43 -0700

    improve windows path management

--- yaml/compiler/workspace_test.go
+++ yaml/compiler/workspace_test.go
@@ -59,3 +59,52 @@ func TestToWindows(t *testing.T) {
 		t.Errorf("Want windows drive %q, got %q", want, got)
 	}
 }
+
+func TestCreateWorkspace(t *testing.T) {
+	tests := []struct {
+		from *yaml.Pipeline
+		base string
+		path string
+		full string
+	}{
+		{
+			from: &yaml.Pipeline{},
+			base: "/drone",
+			path: "src",
+			full: "/drone/src",
+		},
+		{
+			from: &yaml.Pipeline{
+				Workspace: yaml.Workspace{
+					Base: "/foo",
+					Path: "/bar",
+				},
+			},
+			base: "/foo",
+			path: "/bar",
+			full: "/foo/bar",
+		},
+		{
+			from: &yaml.Pipeline{
+				Platform: yaml.Platform{
+					OS: "windows",
+				},
+			},
+			base: "c:\\drone",
+			path: "src",
+			full: "c:\\drone\\src",
+		},
+	}
+	for _, test := range tests {
+		base, path, full := createWorkspace(test.from)
+		if got, want := test.base, base; got != want {
+			t.Errorf("Want workspace base %s, got %s", want, got)
+		}
+		if got, want := test.path, path; got != want {
+			t.Errorf("Want workspace path %s, got %s", want, got)
+		}
+		if got, want := test.full, full; got != want {
+			t.Errorf("Want workspace %s, got %s", want, got)
+		}
+	}
+}
