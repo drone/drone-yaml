199d1f022abb513e31004470ae02da9ead05ad63
Brad Rydzewski <brad.rydzewski@gmail.com>
2019-03-13 14:24:47 -0700

    add convert string

--- yaml/converter/convert_oss.go
+++ yaml/converter/convert_oss.go
@@ -21,3 +21,9 @@ package converter
 func Convert(d []byte, m Metadata) ([]byte, error) {
 	return d, nil
 }
+
+// ConvertString converts the yaml configuration file from
+// the legacy format to the 1.0+ format.
+func ConvertString(s string, m Metadata) (string, error) {
+	return s, nil
+}
